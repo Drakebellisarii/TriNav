cb185ab801faa221b0585e20fdb4c73a8f9a3439
Drakebellisarii <144170639+Drakebellisarii@users.noreply.github.com>
2025-10-22 13:54:24 -0400

    Initial Commit

---
+++ TrinNav/NavigationOptionsView.swift
@@ -0,0 +1,70 @@
+
+import SwiftUI
+
+struct NavigationOptionsView: View {  // Changed from CampusMapView
+    let data: CampusMapData
+    @State private var dragOffset: CGSize = .zero
+    @State private var lastDragValue: DragGesture.Value?
+
+    var body: some View {
+        GeometryReader { geo in
+            let mapW = CGFloat(data.mapWidth)
+            let mapH = CGFloat(data.mapHeight)
+            let imageAspect = mapW / mapH
+            let viewAspect = geo.size.width / geo.size.height
+
+            // Fit the map image inside the view, preserving aspect ratio
+            let fittedWidth: CGFloat
+            let fittedHeight: CGFloat
+
+            if imageAspect > viewAspect {
+                fittedWidth = geo.size.width
+                fittedHeight = geo.size.width / imageAspect
+            } else {
+                fittedHeight = geo.size.height
+                fittedWidth = geo.size.height * imageAspect
+            }
+
+            let scaleX = fittedWidth / mapW
+            let scaleY = fittedHeight / mapH
+
+            ZStack(alignment: .topLeading) {
+                Image(data.mapImageName)
+                    .resizable()
+                    .frame(width: fittedWidth, height: fittedHeight)
+                    .offset(dragOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                if let last = lastDragValue {
+                                    let dx = value.translation.width - last.translation.width
+                                    let dy = value.translation.height - last.translation.height
+                                    dragOffset.width += dx
+                                    dragOffset.height += dy
+                                }
+                                lastDragValue = value
+                            }
+                            .onEnded { _ in
+                                lastDragValue = nil
+                            }
+                    )
+                    .onTapGesture { location in
+                        print("Tapped coordinates: \(location)")
+                    }
+
+                // Draw nodes
+                ForEach(data.nodes) { node in
+                    let posX = CGFloat(node.pixelX) * scaleX + dragOffset.width
+                    let posY = CGFloat(node.pixelY) * scaleY + dragOffset.height
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 16, height: 16)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                        .position(x: posX, y: posY)
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            .clipped()
+        }
+    }
+}
---
+++ TrinNav/TrinNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinNavApp.swift
+//  TrinNav
+//
+//  Created by Drake Bellisari on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TrinNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
